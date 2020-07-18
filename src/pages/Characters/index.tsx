0192c60ce2a5769f0a97c85eeaa91daa614ecd0d
José <josejustinno@gmail.com>
2020-07-18 14:10:11 -0300

    add: configure endpoint to test de request

--- src/pages/Characters/index.tsx
+++ src/pages/Characters/index.tsx
@@ -1,30 +1,47 @@
-import React from 'react';
+import React, { useState, Component } from 'react';
+import api from '../../services/api'
 
 import Header from '../../components/Header';
 import Menu from '../../components/Menu';
 
 import './styles.css';
 
-const Character = () => {
-   return (
-      <>
-         <div className="container-page">
-            <Header />
-            <div id="page">
-               <Menu />
-               <div className="content">
-                  <main>
-                     <h1>Marvel Comics</h1>
-                     <p>
-                        Encontre seu herói favorito, capas de revistas,
-                        crossovers e quadrinhos em um estalar de dedos.
-                     </p>
-                  </main>
+class Character extends Component {
+   state = {
+      characters: []
+   }
+
+   async componentDidMount() {
+      const response = await api.get('/v1/public/characters?limit=10&apikey=6f39d34be067f5aa384594f28f8d9203');
+
+      this.setState({
+         characters: response.data
+      });
+   }
+
+   render() {
+
+      return (
+         <>
+            <div className="container-page">
+               <Header />
+               <div id="page">
+                  <Menu />
+                  <div className="content">
+                     <main>
+                        <h1>Marvel Comics</h1>
+                        <p>
+                           Encontre seu herói favorito, capas de revistas,
+                           crossovers e quadrinhos em um estalar de dedos.
+                        </p>
+                        <p>{this.state.characters}</p>
+                     </main>
+                  </div>
                </div>
             </div>
-         </div>
-      </>
-   );
+         </>
+      );
+   }
 }
 
 export default Character;
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://gateway.marvel.com/v1/public'
+    baseURL: 'http://gateway.marvel.com/'
 })
 
 export default api;
